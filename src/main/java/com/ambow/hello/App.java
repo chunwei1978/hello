6bd49900899e17e8944698133642fac715025aca
chunwei1978 <chunwei.zhao@hotmail.com>
2017-02-17 09:43:44 +0800

    测试用例

---
+++ src/main/java/com/ambow/hello/App.java
@@ -0,0 +1,13 @@
+package com.ambow.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ambow/hello/AppTest.java
@@ -0,0 +1,14 @@
+package com.ambow.hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
